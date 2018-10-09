d77f97c2d37584c9e5c05c3a258fa86cc16c0f5c
yasminehadjeres <yh@big-mama.io>
2018-10-09 22:46:06 +0100

    Les 4 premiers points du tp sans le plot du dataset

--- TP1/first.py
+++ TP1/first.py
@@ -1,12 +1,16 @@
 import weka.core.jvm as jvm
-
 from weka.core.converters import Loader
-from weka.core.dataset import Instances
-from weka.core.dataset import Instance
-from weka.core.dataset import Attribute
 
 
 jvm.start()
-loader = Loader("weka.core.converters.ArffLoader")
-dataset = loader.load_file("/Users/slash/weka-3-8-3/data/iris.arff")
-print(dataset)
+l = Loader("weka.core.converters.ArffLoader")
+d = l.load_file("/Users/slash/weka-3-8-3/data/iris.arff")
+print(d)
+print(d.summary(d))
+print("Dataset summary")
+print("\t \t name   type")
+
+for i in range(d.num_attributes):
+	print(d.attribute(i))
+print("the number of attributes :"+str(d.num_attributes))
+print("the number of instances :"+str(d.num_instances))
